e41ae724c014a3c6d10b6e55e4260544afe491f1
gsemily <gsemily22@gmail.com>
2025-06-11 22:37:22 +0900

    레벨 1-3: Validation

--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package org.example.expert.domain.user.service;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.config.PasswordEncoder;
 import org.example.expert.domain.common.exception.InvalidRequestException;
@@ -9,8 +10,10 @@ import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 @Service
+@Validated
 @RequiredArgsConstructor
 public class UserService {
 
@@ -24,13 +27,7 @@ public class UserService {
     }
 
     @Transactional
-    public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
-        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
-                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
-                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
-            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
-        }
-
+    public void changePassword(long userId, @Valid UserChangePasswordRequest userChangePasswordRequest) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new InvalidRequestException("User not found"));
 
